5f49f5fbb62405e8d7cdc5017a0dc578147f8f35
Kireenko Olga <kireenko86.osil@mail.ru>
2021-04-06 13:48:07 +0500

    edition

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "3545886583600554";
+        String number = "36642470057263";
         System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
     }
 
